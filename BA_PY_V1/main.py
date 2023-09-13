b081c22a6ac5fa2d378f834d79325b91b8eabae4
Nico Manke <nicomanke.de@gmail.com>
2023-09-13 08:54:09 +0200

    - chg: refactoring and formatting - add: vcs.xml (hopefully this shouldn't be in the .gitignore)

--- BA_PY_V1/main.py
+++ BA_PY_V1/main.py
@@ -7,6 +7,7 @@ quests = []
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 model = "gpt-3.5-turbo-0613"
+system_role = "system"
 
 data_structure = {
     "Name": "name of quest",
@@ -33,19 +34,24 @@ data_structure = {
 }
 json_structure = json.dumps(data_structure, ensure_ascii=False).replace('"', r'\"')
 narrative = '''The village of Eich has 20 inhabitants. To the north of Eich lies the Blue Mountains. In the Blue Mountains, there is an old path that leads to a bandit camp and over a badly damaged old bridge. At the end of this path lies Dwarf Mountain, where the great blue dragon Smaug lives. Smaug conquered this mountain for himself many decades ago and drove away the dwarves who once lived there. Since then, Smaug has been considered the archenemy of all dwarves. Behind the bandit camp, there is also a secret dungeon teeming with dangerous creatures and great treasures. Between Eich and the Blue Mountains are the fields of the local farmers. In the east of Eich, there are also many fields, as well as orchards. In the east of Eich, you will also find the Green Meadows, a habitat inhabited by thousands of free-roaming horses, cattle, pigs, and sheep. However, it is also home to large wolves, each as big as a stallion, which pose a great danger to humans as well. According to reports, a few of these wolves have been recently spotted near Eich. Some of the residents of Eich include Siegfried, Tom, Hector, and Markus. Siegfried is also known as the brave dragon slayer. He earned this title after defeating a wyvern in a distant land called Astasia, which is far to the south of Eich. Siegfried is a sociable fellow and always ready for the next battle against a dragon or wyvern. Tom is a farmer who tends to some of the northern fields and also has his own apple orchard in the east. Tom works hard and has a simple way of speaking due to his limited education. Recently, one of his fields in the north was burned down by Smaug. Although most of his fields were spared, Tom is very upset. He now fears that Smaug may target his remaining northern fields as well. Markus is a skilled craftsman capable of repairing various objects and structures. Perhaps Markus could repair the rickety old bridge in the Blue Mountains. Finally, there is Mayor Hector, who is a plump and slow-moving fellow but by no means mentally slow. Hector is clever and a good leader of the village of Birk. He interacts frequently with the villagers and is responsible for assigning new quests, even if they arise at the request of the citizens.'''
-instructions = '''You are now a generator of video game quests for a role-playing game. Generate the quest only in the provided JSON structure and only when explicitly requested to do so. Always give the player the option to accept or decline a quest in the dialogue. The NPC offering the quest should react according to the player's answer. The player character is a stranger who arrives in the village and is not identical to any of the NPCs mentioned in the narrative. In the quest, exclusively use NPCs, locations, items, and factions that are known to you from the given narrative. Do not create new locations, NPCs, items, or factions.'''
+instructions = '''
+You are now a generator of video game quests for a role-playing game. Generate the quest only in the provided JSON structure and only when explicitly
+ requested to do so. Always give the player the option to accept or decline a quest in the dialogue. The NPC offering the quest should react according
+ to the player's answer. The player character is a stranger who arrives in the village and is not identical to any of the NPCs mentioned in the 
+ narrative. In the quest, exclusively use NPCs, locations, items, and factions that are known to you from the given narrative and do not create new 
+ locations, NPCs, items, or factions.'''
 command = "From now on only generate quests if the system or the user explicitly requests you to do so!"
 
 
 def prompt():
     # add quest structure
-    add_message(f"Here is a structure describing a quest for a video rpg game: \n{json_structure}", "system")
+    add_message(f"Here is a structure describing a quest for a video rpg game: \n{json_structure}", system_role)
     # add narrative
-    add_message(f"Here is the narrative of the world our game takes place in: \n{narrative}", "system")
+    add_message(f"Here is the narrative of the world our game takes place in: \n{narrative}", system_role)
     # add clear instructions
-    add_message(instructions, "system")
+    add_message(instructions, system_role)
     # make response only on request
-    add_message(command, "system")
+    add_message(command, system_role)
 
 
 def add_message(message: str, role: str = "user"):
@@ -65,7 +71,7 @@ def get_response():
 
 
 def generate_quest(quest_request: str):
-    add_message(f"Generate a quest for the following player request:\n{quest_request}", "system")
+    add_message(f"Generate a quest for the following player request, using only the given structure:\n{quest_request}", "system")
     request_response = get_response()
     quest = request_response["choices"][0]["message"]["content"]
     quests.append(quest)
