19a6eb3434dd8c60d1535a759b3660d2def5ce33
Nico Manke <nicomanke.de@gmail.com>
2023-09-13 12:27:24 +0200

    - add: move narrative into its own file

---
+++ BA_PY_V1/narrative.py
@@ -0,0 +1,21 @@
+narrative = '''
+The village of Eich has 20 inhabitants. To the north of Eich lies the Blue Mountains. In the Blue Mountains, there is an 
+old path that leads to a bandit camp and over a badly damaged old bridge. At the end of this path lies Dwarf Mountain, 
+where the great blue dragon Smaug lives. Smaug conquered this mountain for himself many decades ago and drove away the 
+dwarves who once lived there. Since then, Smaug has been considered the archenemy of all dwarves. Behind the bandit camp
+, there is also a secret dungeon teeming with dangerous creatures and great treasures. Between Eich and the Blue 
+Mountains are the fields of the local farmers. In the east of Eich, there are also many fields, as well as orchards. In 
+the east of Eich, you will also find the Green Meadows, a habitat inhabited by thousands of free-roaming horses, cattle, 
+pigs, and sheep. However, it is also home to large wolves, each as big as a stallion, which pose a great danger to 
+humans as well. According to reports, a few of these wolves have been recently spotted near Eich. Some of the residents 
+of Eich include Siegfried, Tom, Hector, and Markus. Siegfried is also known as the brave dragon slayer. He earned this 
+title after defeating a wyvern in a distant land called Astasia, which is far to the south of Eich. Siegfried is a 
+sociable fellow and always ready for the next battle against a dragon or wyvern. Tom is a farmer who tends to some of 
+the northern fields and also has his own apple orchard in the east. Tom works hard and has a simple way of speaking due 
+to his limited education. Recently, one of his fields in the north was burned down by Smaug. Although most of his fields 
+were spared, Tom is very upset. He now fears that Smaug may target his remaining northern fields as well. Markus is a 
+skilled craftsman capable of repairing various objects and structures. Perhaps Markus could repair the rickety old 
+bridge in the Blue Mountains. Finally, there is Mayor Hector, who is a plump and slow-moving fellow but by no means 
+mentally slow. Hector is clever and a good leader of the village of Birk. He interacts frequently with the villagers and 
+is responsible for assigning new quests, even if they arise at the request of the citizens.
+'''
