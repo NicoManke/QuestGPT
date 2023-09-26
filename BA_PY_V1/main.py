e317e4d968850864418f4b85e02842d153b8d415
Nico Manke <nicomanke.de@gmail.com>
2023-09-26 17:35:34 +0200

    - chg: better descriptions in the consequence structure

--- BA_PY_V1/main.py
+++ BA_PY_V1/main.py
@@ -27,17 +27,17 @@ data_structure = {
     "SubTasks": [{
         "Name": "name of the sub task",
         "Description": "description of what needs to be done to complete the sub task",
-        "Type": "enum kill, hunt, catch, explore",
+        "Type": "enum kill, hunt, catch, explore, craft",
         "NPC": "name of NPC connected to the subtask",
-        "Location": "name of the location the task takes place",
+        "Location": "name of the location the task takes place at",
         "DialogueOptions": [{
-            "NPC": "name of NPC",
-            "Text": "text",
+            "NPC": "name of the NPC that talks to the player",
+            "Text": "the spoken text",
             "DialogueID": "the dialogue\'s ID, like d_n, where d stands for dialogue and n is the unique number.",
-            "PreviousDialog": "the ID of the previous dialogue that had to be played."
+            "PreviousDialog": "the ID of the previous dialogue that had to be played before arriving at this dialogue."
         }],
         "Task_Consequences": [{
-            "Description": "explaining the changes and consequences for the game world after completing the task."
+            "Description": "Description of the changes to the game world that are the results of the player's actions during the task. For example if the player kills an NPC this NPC's triple \"isAlive\" may be changed to false and other NPCs ma be informed about said death."  # explaining the changes and consequences for the game world after completing the task.
         }]
     }]
 }
@@ -232,18 +232,18 @@ def is_quest_valid(quest_structure: str):
     # print(f"Subtasks:\n{q_sub_tasks}")
 
     # paused until the vAudience Key is available again
-    # i = 1
-    # for task in q_sub_tasks:
-    #   print(f"Task {i}:\n{task}")
-    #   task_consequence = task["Task_Consequences"]
-    #   generate_consequence(task_consequence)
-    #   i = i + 1
-    #   query validity
-    #   how exactly
-    #   1. does the NPC knows everything he talks about?
-    #   2. is the objective (doable) in the named location
-    #   3. is the description valid -> function call? -> use task_consequence = task["Task_Consequences"]
-    #   ...
+    i = 1
+    for task in q_sub_tasks:
+        print(f"Task {i}:\n{task}")
+        task_consequence = task["Task_Consequences"]
+        generate_consequence(task_consequence)
+        i = i + 1
+        # query validity
+        # how exactly
+        # 1. does the NPC knows everything he talks about?
+        # 2. is the objective (doable) in the named location
+        # 3. is the description valid -> function call? -> use task_consequence = task["Task_Consequences"]
+        # ...
     kg = knowledge_graph.KnowledgeGraph("42")
     return kg.validate_quest(quest_structure)
 
