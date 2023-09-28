67108ba8d72bd445f3ed3a29b37c882f58ba785b
Nico Manke <nicomanke.de@gmail.com>
2023-09-28 13:59:33 +0200

    - chg: adjust prompts

--- BA_PY_V1/instructions.py
+++ BA_PY_V1/instructions.py
@@ -5,9 +5,12 @@ def get_instructions():
     decline a quest in the dialogue with an NPC. The NPC offering the quest should react according to the player's answer. 
     The player character is a stranger who arrives in the village and is not identical to any of the NPCs mentioned in the 
     narrative. In the quest, exclusively use NPCs, locations, items, and factions that are known to you from the given 
-    narrative and do not create new locations, NPCs, items, or factions! If a value is null, for example if the task doesn't 
-    have an NPC, then put in "null" as the value. Make sure to put the object keys of the JSON structure always in double 
-    quotes as described in the given JSON structure.'''
+    narrative and do not create new locations, NPCs, items, or factions! Make sure that the story and the tasks of the quest
+    are logical and consistent with the already existing narrative, for example an already dead NPC can't be killed again. 
+    If a conflict in terms of consistency and general logic occurs, change the objective of the quest to find a work-around
+    or implement a plot-twist that solves the logical conflict.
+    If a value is null, for example if the task doesn't have an NPC, then put in "null" as the value. Make sure to put the 
+    object keys of the JSON structure always in double quotes as described in the given JSON structure.'''
     return instructions
 
 
--- BA_PY_V1/quest_structure.py
+++ BA_PY_V1/quest_structure.py
@@ -3,25 +3,27 @@ import json
 
 def get_quest_structure():
     data_structure = {
-        "Name": "name of quest",
+        "Name": "name of the quest",
         "Detailed_Description": "More detailed description explaining exactly what is supposed to happen in the quest.",
-        "Short_Description": "short describing text for the quest that reflects its overall goal",
+        "Short_Description": "shorter version of the Detailed_Description for the quest that reflects its overall goal",
         "Source": "the initial source of the quest. Can be an NPC or a quest board, if available",
         "Chronological": "bool that defines if the sub tasks have to be done in a chronological order or not.",
         "SubTasks": [{
             "Name": "name of the sub task",
-            "Description": "description of what needs to be done to complete the sub task",
-            "Type": "enum kill, hunt, catch, explore, craft",
-            "NPC": "name of NPC connected to the subtask",
+            "Description": "detailed description of what needs to be done to complete the sub task",
+            "Type": "an enum with the following values: kill, hunt, catch, explore, craft, dialogue",
+            "NPC": "name of the NPC connected to the subtask",
             "Location": "name of the location the task takes place at",
             "DialogueOptions": [{
-                "NPC": "name of the NPC that talks to the player",
-                "Text": "the spoken text",
-                "DialogueID": "the dialogue\'s ID, like d_n, where d stands for dialogue and n is the unique number.",
+                "NPC": "name of the NPC that talks to the player or the player himself, if it's his own dialogue",
+                "Text": "the spoken dialogue either starting the conversation or following the previous dialogue",
+                "DialogueID": "the dialogue's ID, like d_n, where d stands for dialogue and n is the unique number.",
                 "PreviousDialog": "the ID of the previous dialogue that had to be played before arriving at this dialogue."
             }],
             "Task_Consequences": [{
-                "Description": "Description of the changes to the game world that are the results of the player's actions during the task. For example if the player kills an NPC this NPC's triple \"isAlive\" may be changed to false and other NPCs ma be informed about said death."
+                "Description": '''Description of the changes to the game world that are the results of the player's 
+                actions during the task. For example if the player kills an NPC this NPC's property "isAlive" may be 
+                changed to false and other NPCs may be informed about said death.'''
                 # explaining the changes and consequences for the game world after completing the task.
             }]
         }]
